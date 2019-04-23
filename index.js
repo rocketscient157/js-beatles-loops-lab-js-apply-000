be02a8ea6db5f94a7a19d8d76998eefdb42c5fd8
Asyet Argynbai <aset2012@gmail.com>
2019-04-23 17:01:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // add solution here
 var theBeatlesPlay
 var johnLennon Facts
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
